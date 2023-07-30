68af47efe5bf9969a1da07fb8754d6ce674b1920
Georgiy Petukhov <84680878+Rasko-web@users.noreply.github.com>
2023-07-30 11:54:02 +0300

    After Computer Review

--- ya_news/news/pytest_tests/test_content.py
+++ ya_news/news/pytest_tests/test_content.py
@@ -10,28 +10,6 @@ from news.models import News, Comment
 User = get_user_model()
 
 
-class TestContent(TestCase):
-
-    @classmethod
-    def setUpTestData(cls):
-        all_news = []
-        for index in range(settings.NEWS_COUNT_ON_HOME_PAGE + 1):
-            news = News(title=f'Новость {index}', text='Просто текст.')
-            all_news.append(news)
-        News.objects.bulk_create(all_news)
-
-    def test_news_count(self):
-        # Загружаем главную страницу.
-        response = self.client.get(self.HOME_URL)
-        # Код ответа не проверяем, его уже проверили в тестах маршрутов.
-        # Получаем список объектов из словаря контекста.
-        object_list = response.context['object_list']
-        # Определяем длину списка.
-        news_count = len(object_list)
-        # Проверяем, что на странице именно 10 новостей.
-        self.assertEqual(news_count, settings.NEWS_COUNT_ON_HOME_PAGE)
-
-
 class TestHomePage(TestCase):
     HOME_URL = reverse('news:home')
 
