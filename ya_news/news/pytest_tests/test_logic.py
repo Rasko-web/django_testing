766d286f7c5b9971828ddef18108e8ffe8de3fab
Georgiy Petukhov <84680878+Rasko-web@users.noreply.github.com>
2023-08-14 02:08:09 +0300

    144

--- ya_news/news/pytest_tests/test_logic.py
+++ ya_news/news/pytest_tests/test_logic.py
@@ -58,28 +58,30 @@ def test_author_can_edit_comment(
 
 
 @pytest.mark.django_db
-def test_other_user_cant_edit_note(
-        admin_client,
-        form_data,
-        pk_for_args,
-        comment):
-    "Пользователь не может редактировать чужой комментарий"
-    url = reverse('news:edit', args=(pk_for_args,))
-    response = admin_client.post(url, form_data)
-    assert response.status_code == HTTPStatus.NOT_FOUND
-    # Получаем новый объект запросом из БД.
-    comment_from_db = Comment.objects.get(id=pk_for_args)
-    assert comment.text == comment_from_db.text
+def test_author_can_delete_comment(author_client, news, comment):
+    "Автор комментария может удалить свой комментарий"
+    url = reverse('news:delete', args=(news.id,))
+    response = author_client.post(url)
+    redirect = (reverse('news:detail', args=(news.id,))) + '#comments'
+    assertRedirects(response, redirect)
+    comment.refresh_from_db()
+    commets_count = Comment.objects.count()
+    assert commets_count == 0
 
 
 @pytest.mark.django_db
-def test_author_can_delete_comment(author_client, pk_for_args):
-    "Автор комментария может удалить свой комментарий"
-    url = reverse('news:delete', args=pk_for_args)
-    response = author_client.post(url)
-    assertRedirects(response, reverse('notes:success'))
-    commets_count = Comment.objects.count()
-    assert commets_count == 0
+def test_other_user_cant_edit_note(
+        admin_client,
+        form_data,
+        news,
+        comment):
+    "Пользователь не может редактировать чужой комментарий"
+    url = reverse('news:edit', args=(news.id,))
+    response = admin_client.post(url, form_data)
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    # Получаем новый объект запросом из БД.
+    comment_from_db = Comment.objects.get(id=news.id)
+    assert comment.text == comment_from_db.text
 
 
 @pytest.mark.django_db
