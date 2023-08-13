084adf1c560810b5e9fd9d4da8f49adbfca018e8
Georgiy Petukhov <84680878+Rasko-web@users.noreply.github.com>
2023-08-14 00:24:43 +0300

    135

--- ya_news/news/pytest_tests/test_routes.py
+++ ya_news/news/pytest_tests/test_routes.py
@@ -1,6 +1,5 @@
 from http import HTTPStatus
 from django.urls import reverse
-from pytest_django.asserts import assertRedirects
 import pytest
 
 
@@ -42,16 +41,3 @@ def test_pages_availability_for_different_users(
     url = reverse(name, args=(comment.id,))
     response = parametrized_client.get(url)
     assert response.status_code == expected_status
-
-
-@pytest.mark.parametrize(
-    'name'
-    ('news:edit', 'news:delete'),
-)
-def test_redirects(admin_client, name, news):
-    # Тест на перенаправление анонимного пользователя на страницу логина
-    login_url = reverse('users:login')
-    url = reverse(name, args=(news.id,))
-    expected_url = f'{login_url}?next={url}'
-    response = admin_client.get(url)
-    assertRedirects(response, expected_url)
