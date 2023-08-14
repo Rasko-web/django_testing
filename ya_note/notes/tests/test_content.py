bd57dab03057709dfa3a0927fd8bc997c24be064
Georgiy Petukhov <84680878+Rasko-web@users.noreply.github.com>
2023-08-14 13:06:37 +0300

    162

--- ya_note/notes/tests/test_content.py
+++ ya_note/notes/tests/test_content.py
@@ -33,3 +33,27 @@ class TestNote(TestCase):
         url = reverse('notes:edit', args=(self.notes.id,))
         response = self.client.get(url)
         self.assertIn('from', response.context)
+
+    def test_note_in_context(self):
+        "отдельная заметка передаётся на страницу со списком заметок в списке"
+        users_statuses = (
+            (self.author, True),
+            (self.reader, False)
+        )
+        for user, status in users_statuses:
+            self.client.force_login(user)
+            url = reverse('notes:list')
+            response = self.client.get(url)
+            object_list = response.context['object_list']
+            self.assertIn(self.notes, object_list) is status
+
+    def test_notes_by_one_author(self):
+        url = reverse("notes:add")
+        self.client.force_login(self.author)
+        response = self.client.post(url, data=self.form_data)
+        self.assertRedirects(response, reverse('notes:success'))
+        notes_count = Note.objects.count()
+        self.assertEqual(notes_count, 1)
+        new_note = Note.objects.get()
+        self.assertEqual(new_note.author, self.author)
+        self.assertEqual(new_note.text, self.form_data['text'])
