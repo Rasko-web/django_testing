f8db05c7a4b94887949da24ceb625c930db0932f
Georgiy Petukhov <84680878+Rasko-web@users.noreply.github.com>
2023-08-14 13:37:15 +0300

    165

--- ya_note/notes/tests/test_logic.py
+++ ya_note/notes/tests/test_logic.py
@@ -1,5 +1,5 @@
 from http import HTTPStatus
-
+from pytils.translit import slugify
 from django.contrib.auth import get_user_model
 from django.test import Client, TestCase
 from django.urls import reverse
@@ -52,6 +52,7 @@ class TestNoteEditDelite(TestCase):
             text='Текст',
             author=cls.author
         )
+        cls.form_data = {'text': cls.NOTE_TEXT}
         cls.reader = User.objects.create(username='Читатель')
         cls.author_client = Client()
         cls.author_client.force_login(cls.author)
@@ -69,3 +70,25 @@ class TestNoteEditDelite(TestCase):
                     url = reverse(name, args=(self.note.slug,))
                     response = user.get(url)
                     self.assertEqual(response.status_code, status)
+
+    def test_empty_slug(self):
+        "Тест на пустой Slug"
+        url = reverse('notes:add')
+        self.form_data.pop(self.note.slug)
+        response = self.author_client.post(url, data=self.form_data)
+        self.assertRedirects(response, reverse('notes:success'))
+        notes_count = Note.objects.count()
+        self.assertEqual(notes_count, 1)
+
+        self.new_note = Note.objects.get()
+        self.expected_slug = slugify(self.form_data['title'])
+        self.assertEqual(self.new_note.slug, self.expected_slug)
+
+    def test_not_unique_slug(self):
+        "Тест на неуникальный Slug"
+        url = reverse('notes:add')
+        self.form_data[self.note.slug] = self.note.id
+        response = self.author_client.get(url, data=self.form_data)
+        self.assertRedirects(response, reverse('notes:add'))
+        note_count = Note.objects.count()
+        self.assertEqual(note_count, 1)
