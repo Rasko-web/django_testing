491f91eb30130a4be00f97d64c8c2023dd458e02
Georgiy Petukhov <84680878+Rasko-web@users.noreply.github.com>
2023-08-21 13:22:23 +0300

    After Review2

--- ya_note/notes/tests/test_content.py
+++ ya_note/notes/tests/test_content.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 from notes.models import Note
@@ -12,38 +12,39 @@ class TestNote(TestCase):
     def setUpTestData(cls):
         cls.author = User.objects.create(username="Автор")
         cls.reader = User.objects.create(username="Читатель")
+
         cls.notes = Note.objects.create(
             title='Заголовок',
             text='Текст',
             slug='note_slug',
             author=cls.author
         )
+        cls.author_client = Client()
+        cls.author_client.force_login(cls.author)
+        cls.reader_client = Client()
+        cls.reader_client.force_login(cls.reader)
         cls.form_data = {'text': "Text"}
 
     def test_author_have_form_on_edit(self):
-        self.client.force_login(self.author)
         url = reverse('notes:edit', args=(self.notes.slug,))
-        response = self.client.get(url)
+        response = self.author_client.get(url)
         self.assertIn('form', response.context)
 
     def test_author_have_form_on_add(self):
-        self.client.force_login(self.author)
         url = reverse('notes:add')
-        response = self.client.get(url)
+        response = self.author_client.get(url)
         self.assertIn('form', response.context)
 
     def test_note_in_context(self):
         "отдельная заметка передаётся на страницу со списком заметок в списке"
-        self.client.force_login(self.author)
         url = reverse('notes:list')
-        response = self.client.get(url)
+        response = self.author_client.get(url)
         object_list = response.context['object_list']
         self.assertIn(self.notes, object_list)
 
     def test_notes_by_one_author(self):
         "Нет заметок от другого пользователя"
         url = reverse("notes:list")
-        self.client.force_login(self.reader)
-        response = self.client.get(url)
+        response = self.reader_client.get(url)
         object_list = response.context['object_list']
         self.assertNotIn(self.notes, object_list)
